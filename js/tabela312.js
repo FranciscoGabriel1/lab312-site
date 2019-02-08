311f1f4fb382af2ab725d020187dee9a880873f0
Francisco Gabriel <franciscogabriel.am@gmail.com>
2019-02-08 10:12:53 -0400

    QuemSomos+Projetos+Publicações (prontos)

--- js/tabela312.js
+++ js/tabela312.js
@@ -1,27 +1,18 @@
-$(document).ready(function() {
-    $(".search").keyup(function () {
-        var searchTerm = $(".search").val();
-        var listItem = $('.results tbody').children('tr');
-        var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
-
-        $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-            return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-        }
+$(function(){
+    $("#tabela input").keyup(function(){
+        var index = $(this).parent().index();
+        var nth = "#tabela td:nth-child("+(index+1).toString()+")";
+        var valor = $(this).val().toUpperCase();
+        $("#tabela tbody tr").show();
+        $(nth).each(function(){
+            if($(this).text().toUpperCase().indexOf(valor) < 0){
+                $(this).parent().hide();
+            }
         });
+    });
 
-        $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
-            $(this).attr('visible','false');
-        });
-
-        $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-            $(this).attr('visible','true');
-        });
-
-        var jobCount = $('.results tbody tr[visible="true"]').length;
-        $('.counter').text(jobCount + ' item');
-
-        if(jobCount == '0') {$('.no-result').show();}
-        else {$('.no-result').hide();}
+    $("#tabela input").blur(function(){
+        $(this).val("");
     });
 });
 
